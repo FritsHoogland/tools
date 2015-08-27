138d7ffe155d3bcf4c5f10e2799652731b513081
FritsHoogland <frits.hoogland@gmail.com>
2015-08-27 14:33:51 +0200

    changed who.sql to be extremely versatile

--- sql/who.sql
+++ sql/who.sql
@@ -1,30 +1,38 @@
 set pagesize 999
 set lines 999
+set verify off
 col username format a13
 col inst format a5
 col sid_serial_inst format a15
 col ospid format a8
-col prog format a40 trunc
-col sid format 9999
-col child for 99999
-col spid format 999999
-col module format a40
-col action format a20
-col client_info format a30
-select /*+ rule as.sql */ a.sid||','||a.serial#||',@'||a.inst_id as sid_serial_inst, 
-	d.spid as ospid, 
-	a.program prog, 
-	a.module, a.action, a.client_info,
-	a.server, a.username
---	'SQL:'||b.sql_id as sql_id, child_number child, plan_hash_value, executions execs,
---	(elapsed_time/decode(nvl(executions,0),0,1,executions))/1000000 avg_etime,
---	decode(a.plsql_object_id,null,sql_text,(select distinct sqla.object_name||'.'||sqlb.procedure_name from dba_procedures sqla, dba_procedures sqlb where sqla.object_id=a.plsql_object_id and sqlb.object_id = a.plsql_object_id and a.plsql_subprogram_id = sqlb.subprogram_id)) sql_text, 
---	(c.wait_time_micro/1000000) wait_s, 
---	decode(a.plsql_object_id,null,decode(c.wait_time,0,decode(a.blocking_session,null,c.event,c.event||'> Blocked by (inst:sid): '||a.final_blocking_instance||':'||a.final_blocking_session),'ON CPU:SQL'),(select 'ON CPU:PLSQL:'||object_name from dba_objects where object_id=a.plsql_object_id)) as wait_or_cpu
-from gv$session a, gv$session_wait c, gv$process d
+-- trickery to suppress asking for the first parameter if not provided.
+-- don't worry: if you don't get it, I got it off the internet, I did not came up with it myself.
+set termout off
+col p1 new_value 1
+select null p1 from dual where 1=2;
+select nvl('&1','short') p1 from dual;
+set termout on
+select
+-- things we always want
+	a.sid||','||a.serial#||',@'||a.inst_id as sid_serial_inst,
+	d.spid as ospid,
+	a.username as username,
+	a.server as server,
+	a.program as program,
+-- case statements for the extra column
+	case lower('&1')
+	when 'long' then a.module||','||a.action||','||a.resource_consumer_group
+	when 'active' then 'time:'||round(a.wait_time_micro/1000,2)||'ms,event:'||decode(a.plsql_object_id,null,decode(a.wait_time,0,decode(a.blocking_session,null,a.event,a.event||'> Blocked by (inst:sid): '||a.final_blocking_instance||':'||a.final_blocking_session),'ON CPU:SQL'),(select 'ON CPU:PLSQL:'||object_name from dba_objects where object_id=a.plsql_object_id))||',seq#:'||a.seq#
+	when 'activeall' then 'time:'||round(a.wait_time_micro/1000,2)||'ms,event:'||decode(a.plsql_object_id,null,decode(a.wait_time,0,decode(a.blocking_session,null,a.event,a.event||'> Blocked by (inst:sid): '||a.final_blocking_instance||':'||a.final_blocking_session),'ON CPU:SQL'),(select 'ON CPU:PLSQL:'||object_name from dba_objects where object_id=a.plsql_object_id))||',seq#:'||a.seq#
+	when 'sql' then 'sql_id:'||a.sql_id||',child#:'||a.sql_child_number
+	end extra
+from 	gv$session a,
+	gv$process d
 where 1=1
-and a.sid = c.sid
-and a.inst_id = c.inst_id
 and a.inst_id = d.inst_id
 and a.paddr = d.addr
+and a.sid like case lower('&1') when 'ami' then sys_context('USERENV', 'SID') else '%' end
+and a.type like case when lower('&1') in ('short','long','active','sql') then 'USER' else '%' end
+and a.type like case lower('&1') when 'bg' then 'BACKGROUND' else '%' end
 /
+undef 1
